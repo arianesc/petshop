0893dd944b9a6f3acad9c71535c5014bdca2c4b2
Ariane <arianesilveira97@hotmail.com>
2019-07-13 14:48:45 -0300

    add pagina welcome

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -69,6 +69,6 @@ class BreedsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def breed_params
-      params.require(:breed).permit(:name)
+      params.require(:breed).permit(:name, :detail)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  get 'welcome/index'
   resources :breeds
   resources :especies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
